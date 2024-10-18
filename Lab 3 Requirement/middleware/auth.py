729d706f4d9234d4f52af9b85229c103058b94a4
SaifAbushanab <155182981+SaifAbushanab@users.noreply.github.com>
2024-10-18 19:08:35 +0300

    Delete Lab 3 Requirement directory

--- Lab 3 Requirement/middleware/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import request, jsonify
-
-# Hardcoded token for simplicity
-TOKEN = "mysecrettoken"
-
-def authenticate_token():
-    token = request.headers.get("Authorization")
-    if not token or token != f"Bearer {TOKEN}":
-        return jsonify({"error": "Unauthorized"}), 401
